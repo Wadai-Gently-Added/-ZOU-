c9a27df4ddafa737a2ca2c811af521d218884c31
Wadai-Gently-Added <wadai.ai.tool@gmail.com>
2026-08-28 16:48:46 +0900

    Delete extension directory

--- extension/edge/background.js
+++
@@ -1,2 +0,0 @@
-// background - PWAへのリレー用ダミー
-chrome.runtime.onMessage.addListener((msg)=>{console.log("[HOU BG]", msg);});
--- extension/safari/README.md
+++
@@ -1,2 +0,0 @@
-# Safari拡張雛形
-Edge版のcontent.jsを流用、XcodeでSafari Web Extensionとして署名必要。Manusさんの指摘通り実機検証必須。
